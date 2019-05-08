7e92930cc4cd85e61dc1a4806f615b5612b84e6f
Ariel Pasilang <ariel.pasilang@Ariels-iMac.local>
2019-05-08 17:48:00 +0800

    updated color

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,8 +37,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#163b2f`,
+        theme_color: `#163b2f`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
